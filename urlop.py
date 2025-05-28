b54b9f8d0556de31a1408cfe01fa3fd559b5147c
aletrev3 <revalt01@outlook.com>
2025-05-28 18:13:23 +0000

    wiki1

--- urlop.py
+++ urlop.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup 
 from urllib.request import urlopen 
+import sys
 
 url = "https://es.wikipedia.org/wiki/Historia_de_M%C3%A9xico"
 page = urlopen(url)
 html = page.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 
-print (soup.get_text())
+with open ('collect.txt', 'w') as f:
+    print (soup.get_text(), file=f)
